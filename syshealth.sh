b8897d21d36c54bcfa642ed41395aaf9caaa9d41
jgr68 <john.ryan@rutgers.edu>
2014-07-03 12:54:13 -0400

    Fundamental design changes

--- syshealth.sh
+++ syshealth.sh
@@ -3,9 +3,15 @@
 # state flags
 CHECK_NIC=false
 CHECK_DISK_USAGE=false
-CHECK_PORT_LISTEN=false
-PORT=0;
+CHECK_LSERVICES=false
+CHECK_NETOWRK=false
+DEVICE=0;
 
+function duplicate_args_err {
+
+	echo "You already entered $1 as an argument.";
+	echo "Can't specify arguments twice.";
+}
 # check to see if the number of arguments is valid
 if [ "$#" -lt 1 ]; then
 	echo "Not enough arguments.";
@@ -18,27 +24,29 @@ fi
 # parse command line arguments
 for (( i=1; i <= $# ; ++i )); do
 	case "${!i}" in
-	"-p")
-		# the argument immediately following the -p flag must be a port number
-		next=`expr $i + 1`;
-		if ! [[ ${!next} =~ ^[0-9]+$ ]] || [ "$i" -eq "$#" ]; then
-			echo "You must specify a port number after -p";
-			echo "Exiting.";
-			exit 1;
-		elif [ $CHECK_PORT_LISTEN = true ]; then
-			echo "Can't specify options twice.";
-			echo "Exiting.";
-			exit 1;
+	"-i")
+		if [ CHECK_NIC = true ]; then
+			duplicate_args_err ${!i};
 		fi
-		CHECK_PORT_LISTEN=true;
-		PORT=${!next};
-		i=$next;
-		;;
-	"-d")
-		CHECK_DISK_USAGE=true;
+		CHECK_NIC=true;
 		;;
 	"-n")
-		CHECK_NIC=true;
+		if [ CHECK_NETWORK = true ]; then
+			duplicate_args_err ${!i};
+		fi
+		CHECK_NETWORK=true;
+		;;
+	"-d")
+		if [ CHECK_DISK_USAGE = true ]; then
+			duplicate_args_err ${!i};
+		fi
+		CHECK_DISK_USAGE=true;
+		;;
+	"-l")
+		if [ CHECK_LSERVICES = true ]; then
+			duplicate_args_err ${!i};
+		fi
+		CHECK_LSERVICES=true;
 		;;
 	*)
 		echo "Invalid option: ${!i}";
@@ -49,25 +57,48 @@ for (( i=1; i <= $# ; ++i )); do
 done
 
 # output state information
+echo;
 [[ $CHECK_NIC = true ]] && status_message="ON" ||  status_message="OFF";
 echo "check_nic_health => $status_message";
 [[ $CHECK_DISK_USAGE = true ]] && status_message="ON" || status_message="OFF";
 echo "check_disk_usage => $status_message";
-[[ $CHECK_PORT_LISTEN = true ]] && status_message="ON" || status_message="OFF";
-echo "check_port_listen => $status_message";
+[[ $CHECK_LSERVICES = true ]] && status_message="ON" || status_message="OFF";
+echo "check_lservices => $status_message";
+[[ $CHECK_NETWORK = true ]] && status_message="ON" || status_message="OFF";
+echo "check_NETWORK => $status_message";
+echo;
 
-if [ $CHECK_PORT_LISTEN = true ]; then
+# display listening services
+if [ $CHECK_LSERVICES = true ]; then
+	
+	echo "PRINTING LISTENING SERVICES";
+	echo;
 
-	# CHECK IF SPECIED PORT IS LISTENING
-	
-	# query netstat for the specified port
-	result=`netstat -plnt | grep "0:$PORT "`;
-	
-	# check if port is listening
+	result=`lsof -i | grep LISTEN`;
 	if [ -n "$result" ]; then
-		service=`echo "$result" | awk '{print $7}' | cut -d'/' -f2`;
-		echo "$service is LISTENING on port $PORT";
+		echo "$result";
+		echo;
 	else
-		echo "Port is not listening.";
+		echo "This machine does not appear to be running any services.";
+		echo;
 	fi
+fi
+if [ $CHECK_NIC = true ]; then
+	devices=`ip link show | awk 'NR%2==1' | awk '{ print $2 }' | sed 's/://'`;
+	for d in $devices; do
+		result=`ethtool "$d"`;
+		echo "PRINTING INFORMATION FOR DEVICE: $d";
+		echo;
+		echo "    $result";
+		echo;
+		
+	done
+fi
+if [ $CHECK_NETWORK = true ]; then
+	echo "PRINTING NETWORK INFORMATION";
+	echo;
+
+	result=`ifconfig`;
+	echo "    $result";
+	echo;
 fi
